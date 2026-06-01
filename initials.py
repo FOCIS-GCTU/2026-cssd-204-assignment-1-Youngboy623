4e756280118b1e6a68dfbcdaaa4b736c1c661709
Aaron Plange <2425401091@live.gctu.edu.gh>
2026-06-01 00:12:11 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,38 @@
+# File: initials.py
+# Description: Print out my initials in stylized large letters.
+# Assignment Number: 1
+#
+# Name: Clifford Banangsana
+# STUDENT ID: 2425401432
+# Email: 2425401432@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor,Clifford Banangsana , this programming
+# assignment is my own work
+# and I have not provided this code to any other student.
+
+
 
 def main():
-  pass #code goes here
 
+    print()
+
+    print("...MCB")
+
+    print()
+
+    print("...MMMM....MMMM............CCCCCCCC........BBBBBBBBBB.......")
+    print("...MMMM....MMMM..........CC................BB........BB.....")
+    print("...MM.MM..MM.MM........CC..................BB........BB.....")
+    print("...MM.MM..MM.MM........CC..................BB........BB.....")
+    print("...MM.MM..MM.MM........CC..................BBBBBBBBBB.......")
+    print("...MM..MMMM..MM........CC..................BB........BB.....")
+    print("...MM..MMMM..MM........CC..................BB........BB.....")
+    print("...MM...MM...MM........CC..................BB........BB.....")
+    print("...MM...MM...MM...**.....CC...........**...BB........BB...**")
+    print("...MM...MM...MM...**.......CCCCCCCC...**...BBBBBBBBBB.....**")
+
+    print()
 
 
 main()
