41492c13ec1594256edf26f164d52c475212cc18
Aaron Plange <2425401091@live.gctu.edu.gh>
2026-06-01 00:26:00 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,31 +1,36 @@
- File: initials.py
- Description: Print out my initials in large stylized letters.
- Assignment Number: 2
+# File: initials.py
+# Description: Print out my initials in stylized large letters.
+# Assignment Number: 1
+#
+# Name: Clifford Banangsana
+# STUDENT ID: 2425401432
+# Email: 2425401432@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor,Clifford Banangsana , this programming
+# assignment is my own work
+# and I have not provided this code to any other student.
 
-Name: Clifford Banangsana
-STUDENT ID: 2425401432
-Email: 2425401432@live.gctu.edu.gh
-Grader: Augustus
-
-On my honor, Clifford Banangsana, this programming assignment is my own work and I have not provided this code to any other student.
 
 
 def main():
-  
-    print()
-    print("...CB")
+
     print()
 
-    print("...CCCCCCCCCCCC...BBBBBBBBBBBB...****")
-    print("...CC.............BB........BB...****")
-    print("...CC.............BB........BB...****")
-    print("...CC.............BB........BB...****")
-    print("...CC.............BBBBBBBBBBBB...****")
-    print("...CC.............BB........BB...****")
-    print("...CC.............BB........BB...****")
-    print("...CC.............BB........BB...****")
-    print("...CC.............BB........BB...****")
-    print("...CCCCCCCCCCCC...BBBBBBBBBBBB...****")
+    print("...MCB")
+
+    print()
+
+    print("...MMMM....MMMM............CCCCCCCC........BBBBBBBBBB.......")
+    print("...MMMM....MMMM..........CC................BB........BB.....")
+    print("...MM.MM..MM.MM........CC..................BB........BB.....")
+    print("...MM.MM..MM.MM........CC..................BB........BB.....")
+    print("...MM.MM..MM.MM........CC..................BBBBBBBBBB.......")
+    print("...MM..MMMM..MM........CC..................BB........BB.....")
+    print("...MM..MMMM..MM........CC..................BB........BB.....")
+    print("...MM...MM...MM........CC..................BB........BB.....")
+    print("...MM...MM...MM...**.....CC...........**...BB........BB...**")
+    print("...MM...MM...MM...**.......CCCCCCCC...**...BBBBBBBBBB.....**")
 
     print()
 
